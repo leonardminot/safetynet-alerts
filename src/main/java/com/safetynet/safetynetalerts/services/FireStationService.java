d8470df1ba6ce52a7e43dd9bb2eb946d11f45270
leonard.minot <leonard.minot@gadz.org>
2023-08-25 16:40:13 +0200

    CRUD Firestation : add login on FireStationService for creation of a new mapping

--- src/main/java/com/safetynet/safetynetalerts/services/FireStationService.java
+++ src/main/java/com/safetynet/safetynetalerts/services/FireStationService.java
@@ -3,12 +3,14 @@ package com.safetynet.safetynetalerts.services;
 import com.safetynet.safetynetalerts.exception.ApiResourceException;
 import com.safetynet.safetynetalerts.models.Firestation;
 import com.safetynet.safetynetalerts.repositories.FirestationRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
+@Slf4j
 public class FireStationService {
 
     private final FirestationRepository firestationRepository;
@@ -21,11 +23,15 @@ public class FireStationService {
     public void createMapping(Firestation firestation) {
         firestationRepository.isMappingExist(firestation).ifPresentOrElse(
                 (fs) -> {
+                    log.error(String.format("On POST /firestation : mapping %s already exists", firestation.toString()));
                     throw new ApiResourceException(String.format("mapping address : [%s] with station : [%s] already exist",
                             fs.address(),
                             fs.station()));
                 },
-                () -> firestationRepository.createMapping(firestation)
+                () -> {
+                    log.info(String.format("On POST /firestation : Success for the creation of the mapping %s", firestation.toString()));
+                    firestationRepository.createMapping(firestation);
+                }
         );
 
     }
