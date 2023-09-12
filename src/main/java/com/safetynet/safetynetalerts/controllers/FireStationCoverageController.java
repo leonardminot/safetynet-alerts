adafb1264163ab569900f5f93ffe8ee7e8be11ee
leonard.minot <leonard.minot@gadz.org>
2023-09-12 21:55:39 +0200

    Additionally, imported necessary dependencies and updated setUp/tearDown methods for testing. Created 'getPersonCoverage' method in the controller for handling GET requests returning covered persons by a fire station, distinguished by the count of adults and children. These changes are necessary to ensure that the fire station coverage feature is correctly implemented and tested.

--- src/main/java/com/safetynet/safetynetalerts/controllers/FireStationCoverageController.java
+++ src/main/java/com/safetynet/safetynetalerts/controllers/FireStationCoverageController.java
@@ -1,10 +1,17 @@
 package com.safetynet.safetynetalerts.controllers;
 
+import com.safetynet.safetynetalerts.dto.PersonsCoveredByFirestationDTO;
 import com.safetynet.safetynetalerts.services.FireStationCoverageService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("firestation")
@@ -16,5 +23,19 @@ public class FireStationCoverageController {
     @Autowired
     public FireStationCoverageController(FireStationCoverageService fireStationCoverageService) {
         this.fireStationCoverageService = fireStationCoverageService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<PersonsCoveredByFirestationDTO>> getPersonCoverage(@RequestParam String stationNumber) {
+        List<PersonsCoveredByFirestationDTO> personsCoveredByFirestationDTOS = fireStationCoverageService.getCoverageForAStationNumber(stationNumber);
+        long totalOfAdults = fireStationCoverageService.getTotalAdults(stationNumber);
+        long totalOfChildren = fireStationCoverageService.getTotalChildren(stationNumber);
+
+        HttpHeaders responseHeaders = new HttpHeaders();
+
+        responseHeaders.set("Adults-Count", String.valueOf(totalOfAdults));
+        responseHeaders.set("Children-Count", String.valueOf(totalOfChildren));
+
+        return ResponseEntity.ok().headers(responseHeaders).body(personsCoveredByFirestationDTOS);
     }
 }
--- src/test/java/com/safetynet/safetynetalerts/IntegrationTests/FireStationCoverageIT.java
+++ src/test/java/com/safetynet/safetynetalerts/IntegrationTests/FireStationCoverageIT.java
@@ -1,11 +1,30 @@
 package com.safetynet.safetynetalerts.IntegrationTests;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.safetynet.safetynetalerts.dto.PersonsCoveredByFirestationDTO;
+import com.safetynet.safetynetalerts.mockressources.utils.ManageMockedData;
+import com.safetynet.safetynetalerts.models.Firestation;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import static com.jayway.jsonpath.internal.path.PathCompiler.fail;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -25,5 +44,70 @@ public class FireStationCoverageIT {
         this.filePathMockFireStations = filePathMockFireStations;
         this.filePathMockPersons = filePathMockPersons;
         this.filePathMockMedicalRecords = filePathMockMedicalRecords;
+    }
+
+    @BeforeEach
+    void setUp() throws IOException {
+        ManageMockedData.createPersonMockedData(filePathMockPersons);
+        ManageMockedData.createFirestationsMockedData(filePathMockFireStations);
+        ManageMockedData.createMedicalRecordsMockedData(filePathMockMedicalRecords);
+    }
+
+    @AfterEach
+    void tearDown() throws FileNotFoundException {
+        ManageMockedData.clearJsonFile(filePathMockPersons);
+        ManageMockedData.clearJsonFile(filePathMockFireStations);
+        ManageMockedData.clearJsonFile(filePathMockMedicalRecords);
+    }
+
+    @Test
+    void itShouldGet3PersonsAndReturn2AdultsAnd1Child() throws Exception {
+        // Given
+        PersonsCoveredByFirestationDTO magnus = new PersonsCoveredByFirestationDTO(
+                "Magnus",
+                "Carlsen",
+                "007 Rue de la Dame",
+                "123-456-7890"
+        );
+
+        PersonsCoveredByFirestationDTO miniMagnus = new PersonsCoveredByFirestationDTO(
+                "miniMagnus",
+                "miniCarlsen",
+                "007 Rue de la Dame",
+                null
+        );
+
+        PersonsCoveredByFirestationDTO gari = new PersonsCoveredByFirestationDTO(
+                "Gari",
+                "Kasparov",
+                "105 Rue du Fou",
+                "741-852-9630"
+        );
+
+        List<PersonsCoveredByFirestationDTO> personsCoveredByFirestationDTOS = new ArrayList<>();
+        personsCoveredByFirestationDTOS.add(magnus);
+        personsCoveredByFirestationDTOS.add(gari);
+        personsCoveredByFirestationDTOS.add(miniMagnus);
+        String expectedResult = personsCoveredToJson(personsCoveredByFirestationDTOS);
+
+        // When
+        ResultActions resultActions = mockMvc.perform(MockMvcRequestBuilders.get("/firestation?stationNumber=1")
+                        .contentType(MediaType.APPLICATION_JSON));
+
+        // Then
+
+        resultActions.andExpect(status().isOk())
+                .andExpect(header().string("Adults-Count", "2"))
+                .andExpect(header().string("Children-Count", "1"))
+                .andExpect(content().string(expectedResult));
+    }
+
+    private String personsCoveredToJson(List<PersonsCoveredByFirestationDTO> personsCoveredByFirestationDTO) {
+        try {
+            return new ObjectMapper().writeValueAsString(personsCoveredByFirestationDTO);
+        } catch (JsonProcessingException e) {
+            fail("");
+            return null;
+        }
     }
 }
