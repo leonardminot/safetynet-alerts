0b5714af1c1f51d91c5cd51da5f252f76825324e
leonard.minot <leonard.minot@gadz.org>
2023-10-02 10:15:26 +0200

    Add API documentation for: - Flood Alert

--- src/main/java/com/safetynet/safetynetalerts/controllers/FloodAlertController.java
+++ src/main/java/com/safetynet/safetynetalerts/controllers/FloodAlertController.java
@@ -2,6 +2,9 @@ package com.safetynet.safetynetalerts.controllers;
 
 import com.safetynet.safetynetalerts.dto.FloodAlertDTO;
 import com.safetynet.safetynetalerts.services.FloodAlertService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,6 +17,10 @@ import java.util.List;
 @RestController
 @RequestMapping("flood")
 @Slf4j
+@Tag(name = "Flood Alert",
+        description = """
+This controller provides an endpoint for retrieving information about households served by the specified fire stations. The fire station numbers are provided as a list in the request parameters. The response from this request organizes people by address and includes the name, phone number, and age of the inhabitants, along with their medical history (medication, dosage, and allergies).
+""")
 public class FloodAlertController {
     private final FloodAlertService floodAlertService;
 
@@ -22,6 +29,17 @@ public class FloodAlertController {
         this.floodAlertService = floodAlertService;
     }
 
+    @Operation(
+            description = """
+        Based on the list of fire station numbers provided as request parameters, this endpoint provides information about all households served by these fire stations. The resulting data organizes people by address and includes the name, phone number, age of inhabitants, as well as their medical history (medication, dosage, and allergies).
+        """,
+            responses = {
+                    @ApiResponse(
+                            description = "Successfully retrieved information about all households served by the specified fire stations, along with residents' details and their medical history.",
+                            responseCode = "200"
+                    )
+            }
+    )
     @GetMapping("stations")
     List<FloodAlertDTO> getFloodAlert(@RequestParam List<String> stations) {
         log.info("New request: GET /flood/stations?stations=" + stations);
