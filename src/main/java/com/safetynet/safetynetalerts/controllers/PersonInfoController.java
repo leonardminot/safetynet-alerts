24fc05ed4514f829cfebba3bb03e3a2aeb0e86a3
leonard.minot <leonard.minot@gadz.org>
2023-10-02 10:20:47 +0200

    Add API documentation for: - Person Info

--- src/main/java/com/safetynet/safetynetalerts/controllers/PersonInfoController.java
+++ src/main/java/com/safetynet/safetynetalerts/controllers/PersonInfoController.java
@@ -2,6 +2,9 @@ package com.safetynet.safetynetalerts.controllers;
 
 import com.safetynet.safetynetalerts.dto.PersonInfoDTO;
 import com.safetynet.safetynetalerts.services.PersonInfoService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,6 +17,10 @@ import java.util.List;
 @RestController
 @RequestMapping("personInfo")
 @Slf4j
+@Tag(name = "Person Information",
+        description = """
+        This controller provides an endpoint for retrieving the name, address, age, email, and medical history (medication, dosage, allergies) of a resident. The resident is identified by their firstName and lastName, which are mandatory request parameters.
+        """)
 public class PersonInfoController {
 
     private final PersonInfoService personInfoService;
@@ -23,6 +30,17 @@ public class PersonInfoController {
         this.personInfoService = personInfoService;
     }
 
+    @Operation(
+            description = """
+This endpoint, based on the mandatory firstName and lastName parameters, provides the name, address, age, email, and medical history (medication, dosage, allergies) of a resident.
+""",
+            responses = {
+                    @ApiResponse(
+                            description = "Successfully retrieved the details of the resident.",
+                            responseCode = "200"
+                    )
+            }
+    )
     @GetMapping
     private List<PersonInfoDTO> getPersonInfo(@RequestParam String firstName, @RequestParam String lastName) {
         log.info("New request: GET /personInfo?firstName=" + firstName + "&lastName=" + lastName);
