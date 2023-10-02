95c4f06c5f0a5e03f630e3c4dbf42f7fdfcc92c5
leonard.minot <leonard.minot@gadz.org>
2023-10-02 09:59:19 +0200

    Add API documentation for: - Community Email

--- src/main/java/com/safetynet/safetynetalerts/controllers/CommunityEmailController.java
+++ src/main/java/com/safetynet/safetynetalerts/controllers/CommunityEmailController.java
@@ -1,6 +1,9 @@
 package com.safetynet.safetynetalerts.controllers;
 
 import com.safetynet.safetynetalerts.services.CommunityEmailService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,6 +16,10 @@ import java.util.List;
 @RestController
 @RequestMapping("communityEmail")
 @Slf4j
+@Tag(name = "Community Email",
+        description = """
+This controller provides endpoints for retrieving the email addresses of all the residents of a specific city, given the city name as a request parameter.
+""")
 public class CommunityEmailController {
 
     private final CommunityEmailService communityEmailService;
@@ -21,7 +28,17 @@ public class CommunityEmailController {
     public CommunityEmailController(CommunityEmailService communityEmailService) {
         this.communityEmailService = communityEmailService;
     }
-
+    @Operation(
+            description = """
+        This endpoint, based on a given city parameter, provides the email addresses of all the residents of the specified city.
+        """,
+            responses = {
+                    @ApiResponse(
+                            description = "Successfully retrieved email addresses of all the residents of the specified city.",
+                            responseCode = "200"
+                    )
+            }
+    )
     @GetMapping
     List<String> getCommunityEmail(@RequestParam String city) {
         log.info("New request: GET /communityEmail?city=" + city);
