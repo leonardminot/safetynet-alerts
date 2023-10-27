03d74df82bc91766addae9457158395524660b5f
leonard.minot <leonard.minot@gadz.org>
2023-10-27 16:34:54 +0200

    Simplification of data loading: mock data is managed in each test case. No need for a special command line runner for tests (it could be generating confusion)

--- src/main/java/com/safetynet/safetynetalerts/configuration/LoadInitialDataRunner.java
+++ src/main/java/com/safetynet/safetynetalerts/configuration/LoadInitialDataRunner.java
@@ -3,10 +3,8 @@ package com.safetynet.safetynetalerts.configuration;
 import com.safetynet.safetynetalerts.services.InitialLoadDataService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 
 @Configuration
-@Profile("!dev")
 public class LoadInitialDataRunner implements CommandLineRunner {
 
     private final InitialLoadDataService initialLoadDataService;
